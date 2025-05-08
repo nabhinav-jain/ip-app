0b2ad5d9b599f6d732fbfc85f5c5df1d5e255f4d
abhinav <jabhinav041@gmail.com>
2025-05-08 18:07:31 +0530

    removed tailwind

--- postcss.config.cjs
+++
@@ -1,7 +0,0 @@
-// postcss.config.cjs
-module.exports = {
-  plugins: {
-    '@tailwindcss/postcss': {},  // required in Tailwind v4+ 
-    autoprefixer: {},
-  },
-}
--- tailwind.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  content: [
-    "./index.html",             // your HTML entry
-    "./src/**/*.{js,jsx,ts,tsx}"// all JS/JSX/TSX components
-  ],
-  theme: { extend: {} },
-  plugins: [],
-}
